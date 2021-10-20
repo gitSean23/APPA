6f4eef7dee144ea15587e64bf081da89ea4d223b
Sean Feldman <66270571+gitSean23@users.noreply.github.com>
2021-10-19 21:24:40 -0400

    New methods added

--- bmp280Appa.ino
+++ bmp280Appa.ino
@@ -5,7 +5,7 @@
 #define BMP_SCK  (13)
 #define BMP_MISO (12)
 #define BMP_MOSI (11)
-#define BMP_CS   (10)
+#define BMP_CS   (7)
 
 Adafruit_BMP280 bmp; // I2C
 //Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
@@ -14,6 +14,8 @@ Adafruit_BMP280 bmp; // I2C
 void setup() {
   Serial.begin(9600);
   Serial.println(F("BMP280 test"));
+  SD.begin();
+  cardFile = SD.open();
 
   //if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
   if (!bmp.begin()) {
@@ -31,13 +33,11 @@ void setup() {
 }
 
 void loop() {
-    Serial.print(F("Temperature = "));
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-
-    Serial.print(F("Pressure = "));
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
+    delay(2000);
+    
+    readTemp("f");
+    cardFile.println(logTemp());
+    readPress("pa");
 
     Serial.print(F("Approx altitude = "));
     Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
@@ -45,4 +45,64 @@ void loop() {
 
     Serial.println();
     delay(2000);
+}
+
+//Reads temperature @param option 
+void readTemp(String option){
+  if (option.toLowerCase() == "f"){
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature()*(9/5)+32);
+    Serial.println(" *F");
+  }
+
+ //Logs temperature to SD card @param option @return float temp
+ float logTemp(String option){
+    if(option.toLowerCase() == "f"){
+      fTemp = bmp.readTemperature()*(9/5)+32;
+      return fTemp;
+    }
+
+    else if(option.toLowerCase() == "c"){
+      cTemp = bmp.readTemperature();
+      return cTemp;
+    }
+
+    else if(option.toLowerCase() == "k"){
+      kTemp = bmp.readTemperature()+273.15;
+      return kTemp;
+    }
+ }
+  else if (option.toLowerCase() == "c"){
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+  }
+
+  else if (option.toLowerCase() == "k"){
+    Serial.print(bmp.readTemperature()+273.15)
+    Serial.println(" *K");
+  }
+
+  else{
+    Serial.println("Option not available");
+  }
+}
+
+//Reads pressure @param option 
+void readPress(String option){
+  if (option == "mil"){
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure()/100);
+    Serial.println(" Milibar");
+}
+  else if(option == "pa"){
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+  }
+
+  else if(option == "bar"){
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure()*100000);
+    Serial.println(" Barr");
+  }
 }
