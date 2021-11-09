04f21877fafe2c0f6721ad90af60dbf59c2a1f58
Sean Feldman <66270571+gitSean23@users.noreply.github.com>
2021-11-09 11:23:03 -0500

    Add files via upload

--- bmp280Appa.ino
+++ bmp280Appa.ino
@@ -11,6 +11,12 @@
 
 unsigned long timePassed;
 
+float alt[50];
+float acc[50]; 
+
+int altCount = 0;
+int accCount = 0;
+
 bool apogee = false;
 bool *apogeeptr = &apogee; 
 int count = 0;
@@ -22,19 +28,18 @@ Adafruit_BMP280 bmp; // I2C
 File cardFile;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println(F("BMP280 test"));
-  Serial.println("Initializing SD");
+  Serial.begin(9600); //Sets up the serial port
+  Serial.println("Initializing SD"); 
   if(!SD.begin(BMP_CS)){
     Serial.println("Initialization unsuccessful");
     while(true);
   }
   Serial.println("SD initialization successful!");
-  SD.begin(BMP_CS);
+  SD.begin(BMP_CS); //SD Card starts up
   cardFile = SD.open("SDTest.rtf", FILE_WRITE); //Opens the file
 
   //if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
-  if (!bmp.begin()) {
+  if (!bmp.begin()) { //Checks if a bmp sensor exists
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
                       "try a different address!"));
     while (1) delay(10);
@@ -47,53 +52,25 @@ void setup() {
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
-  timePassed = millis();
+  timePassed = millis(); //Setting up the elapsed time
 }
 
 void loop() {
     delay(2000);
-
-    //bmp.readAltitude();
-    if(bmp.readAltitude() >= 803.00){ //Notify apogee and recovery deployment
-      Serial.print(timePassed);
-      Serial.print(" --> ");
-      Serial.print("Alt: ");
-      Serial.println(bmp.readAltitude());
-      if(count == 1){
-        Serial.println("Apogee Reached!");
-        Serial.println("Recovery Deployed!");
-      }
-      *apogeeptr = true;
-      count++;
-      
-    }
-
-    else if (bmp.readAltitude() <=803.00 and count >=1){ //Perform decsending event
-      Serial.print(timePassed);
-      Serial.print(" --> ");
-      Serial.println(bmp.readAltitude());
-      Serial.println("Descending");
-      digitalWrite(PIN_LED, HIGH);
-      delay(500);
-      digitalWrite(PIN_LED, LOW);
-      delay(500);
-      digitalWrite(PIN_LED, HIGH);
-    }
-
     
-
-    
-    readTemp("far");
-    cardFile = SD.open("SDTest.rtf", FILE_WRITE);
-    cardFile.println(logTemp("far"));
-    cardFile.close();
-    readPress("pa");
+    readTemp("far"); //Prints the temperature in farenheit 
+    cardFile = SD.open("SDTest.rtf", FILE_WRITE); //Sets up a text file to write in
+    logTemp("far");//Takes the value of the temperatures and writes it into the text file
+    alt[altCount] = logTemp("far");
+    readPress("pa"); //Prints the pressure in pascals
 
     //Serial.print(F("Approx altitude = "));
     //Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
     //Serial.println(" m");
 
-    EDL(false, true);
+    EDL(false, true); //Calls the edl function
+    altCount++; 
+    accCount++;
 
     Serial.println();
     delay(2000);
@@ -166,32 +143,32 @@ void readPress(String option){
 
 
 int EDL(bool landed, bool activated){
-  if(bmp.readAltitude() == 50.00 && landed == false && activated){ //Ignition
+  if(bmp.readAltitude() <= 50.00){ //Ignition
     Serial.println("Event --> Ignition");
     return 1;
   }
 
-  else if (bmp.readAltitude() == 65.00 && landed == false && activated){ //Liftoff
+  else if (bmp.readAltitude() >= 65.00 && alt[altCount] < 65.00){ //Liftoff
     Serial.println("Event --> Liftoff!");
     return 2;
   }
 
-  else if ((bmp.readAltitude() >= 115.00 && bmp.readAltitude() <= 120.00) && (landed == false && activated)) { //Motor Burnout
+  else if (bmp.readAltitude() >= 115.00 && alt[count] < 115.00) { //Motor Burnout
     Serial.println("Event --> Burnout");
     return 3;
   }
 
-  else if((bmp.readAltitude() >= 205.00 && bmp.readAltitude() <= 210.00) && (landed == false && activated)){ //Ejection Charge
+  else if(bmp.readAltitude() >= 205.00 && alt[count] < 205.00){ //Ejection Charge
     Serial.println("Event --> Ejection Charge");
     return 4;
   }
 
-  else if ((bmp.readAltitude() == 300.00) && (landed == false && activated)){
+  else if ((bmp.readAltitude() == 300.00) && alt[count] > 300.00){
     Serial.println("Event --> Main Parachute Deployment");
     return 5;
   }
 
-  else if (bmp.readAltitude() == 50.00 && landed == true && activated){
+  else if (bmp.readAltitude() <= 50.00 && alt[count] > 50.00){
     Serial.println("Event --> Landed!");
     return 6;
   }
